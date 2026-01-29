babe7281e3497d6e9485129323b5c80f2f11a7e5
Sukeerthi23 <sukeerthikumar23@gmail.com>
2026-01-29 09:41:03 +0530

    first commit

---
+++ Route/userRoutes.js
@@ -0,0 +1,9 @@
+import { createUserController } from "../Controller/userController.js";
+
+import express from 'express'
+ 
+const userRoute = express.Router();//post put get delete
+
+userRoute.post('/signup', createUserController);
+
+export default userRoute
